11d7b05b3ec9f2aab898f1158e12e009a1238885
Grace Rivas <grace.rivas@shopify.com>
2023-10-26 18:00:53 +0000

    Initializing the tutorials

---
+++ react/example-customer-account--loyalty-app--react/CustomerFulfillmentDetailsDelivery.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+Card,
+BlockStack,
+reactExtension,
+Button,
+useApi,
+TextBlock
+} from "@shopify/ui-extensions-react/customer-account";
+
+export default reactExtension(
+"customer-account.order-status.fulfillment-details.render-after",
+( ) => <CustomerFulfillmentDetailsDelivery />
+);
+
+function CustomerFulfillmentDetailsDelivery( ) {
+  const api = useApi<"customer-account.order-status.fulfillment-details.render-after">()
+  return (
+    <Card padding>
+      <BlockStack>
+        <TextBlock>
+          Tell us how we did for a chance to win 1000 points
+        </TextBlock>
+        <BlockStack maxInlineSize={150}>
+          <Button appearance="monochrome" kind="secondary" >
+            Write a review
+          </Button>
+        </BlockStack>
+     </BlockStack>
+   </Card>
+  );
+}
