b6404d34328b2236e20d9d5db9373396bbbfad99
Robin Drexler <robin.drexler@shopify.com>
2024-03-14 17:35:12 +0000

    use fetch-then-render for menu actions

--- react/example-customer-account--order-action-menu--react/extensions/action-menu-extension-react/src/MenuActionExtension.tsx
+++ react/example-customer-account--order-action-menu--react/extensions/action-menu-extension-react/src/MenuActionExtension.tsx
@@ -5,52 +5,49 @@ import {
   useApi,
 } from "@shopify/ui-extensions-react/customer-account";
 
+
 // [START menu-action.setup-target]
 export default reactExtension(
   "customer-account.order.action.menu-item.render",
-  ( ) => <MenuActionExtension />
-);
-// [END menu-action.setup-target]
-
-function MenuActionExtension( ) {
-  const {orderId} = useApi<"customer-account.order.action.menu-item.render">()
-  const [showAction, setShowAction] = useState(false)
-
-  // [START menu-action.fetch-order-fulfillments]
-  useEffect(() => {
+  async (api) => {
+    const { orderId } = api;
     const orderQuery = {
       query: `query {
         order(id: "${orderId}") {
-          fulfillments(first: 10) {
+          fulfillments(first: 1) {
             nodes {
               latestShipmentStatus
             }
           }
         }
-      }`
+      }`,
     };
+    const result = await fetch(
+      "shopify://customer-account/api/latest/graphql.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderQuery),
+      }
+    );
+    const { data } = await result.json();
+    const hasFulfillments = data.order.fulfillments.nodes.length !== 0;
 
-    fetch("shopify://customer-account/api/latest/graphql.json",
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(orderQuery),
-        }).then((response) => response.json())
-        .then(({data}) => {
-          setShowAction(data.order.fulfillments.nodes.length !== 0)
-        }).catch(console.error);
-  }, [orderId]);
-  // [END menu-action.fetch-order-fulfillments]
+    return <MenuActionExtension showAction={hasFulfillments} />;
+  }
+);
+// [END menu-action.setup-target]
 
+function MenuActionExtension({ showAction }: { showAction: boolean }) {
   // [START menu-action.conditionally-render]
-  if (!showAction) return null;
+  if (!showAction) {
+    return null;
+  }
   // [END menu-action.conditionally-render]
 
   // [START menu-action.render-button]
-  return (
-    <Button>Report a problem</Button>
-  );
+  return <Button>Report a problem</Button>;
   // [END menu-action.render-button]
 }
